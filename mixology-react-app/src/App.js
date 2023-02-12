f2dec307efe6a8870736fd07b63b06b55be3dda6
makoma mabilo <makoma44@gmail.com>
2023-02-12 21:06:10 +0800

    added header

--- mixology-react-app/src/App.js
+++ mixology-react-app/src/App.js
@@ -1,24 +1,9 @@
-
+import Header from './Components/Layout/Header';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-        <p>
-          Hello
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-
-        </a>
-      </header>
-    </div>
+    <Header></Header>
   );
 }
 
