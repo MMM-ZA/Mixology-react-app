245e07c49d419ac81345579e843b0852f62bfb5b
makoma mabilo <makoma44@gmail.com>
2023-03-07 22:46:08 +0800

    populated sectioContents data and added images

--- mixology-react-app/src/Components/Layout/SectionContents.js
+++ mixology-react-app/src/Components/Layout/SectionContents.js
@@ -1,42 +1,43 @@
 import { useState } from "react";
 import Card from "../UI/Card";
+import classes from "./SectionContents.module.css";
 
 const sectionData = [
   {
     id: 's1',
     name: 'Gin',
-    description: 'Finest fish and veggies',
-    backgroundImage: 'url("/path/to/image.jpg")',
+    description: 'Gin is a clear, high-proof spirit that is flavored with botanicals, typically juniper berries, and other herbs and spices. It is often used in cocktails and is known for its distinctive pine and citrus flavors.',
+    backgroundImage: 'url("src/Assets/gin.png")',
   },
   {
     id: 's2',
     name: 'Whiskey',
-    description: 'Finest fish and veggies',
-    backgroundImage: 'url("/path/to/image.jpg")',
+    description: 'Whiskey is a spirit made from fermented grains, typically barley, corn, rye, or wheat, and aged in oak barrels. There are several different types of whiskey, including bourbon, Scotch, and Irish whiskey, each with its own distinct flavor profile.',
+    backgroundImage: 'url("src/Assets/whiskey.png")',
   },
   {
     id: 's3',
-    name: 'Tequilla',
-    description: 'Finest fish and veggies',
-    backgroundImage: 'url("/path/to/image.jpg")',
+    name: 'Tequila',
+    description: 'Tequila is a spirit made from the blue agave plant, which is native to Mexico. It is typically produced in the state of Jalisco and is known for its distinctive smoky and earthy flavors. Tequila is often enjoyed as a shot or mixed in cocktails such as margaritas.',
+    backgroundImage: 'url("src/Assets/tequila.png")',
   },
   {
     id: 's4',
     name: 'Rum',
-    description: 'Finest fish and veggies',
-    backgroundImage: 'url("/path/to/image.jpg")',
+    description: 'Rum is a spirit made from sugarcane or molasses that is fermented and distilled. It is typically aged in oak barrels and has a sweet and slightly spicy flavor. Rum is often used in cocktails such as mojitos and daiquiris.',
+    backgroundImage: 'url("src/Assets/rum.png")',
   },
   {
     id: 's5',
     name: 'Vodka',
-    description: 'Finest fish and veggies',
-    backgroundImage: 'url("/path/to/image.jpg")',
+    description: 'Vodka is a clear, neutral-tasting spirit that is made from fermented grains, potatoes, or other starches.',
+    backgroundImage: 'url("src/Assets/vodka.png")',
   },
   {
     id: 's6',
     name: 'Brandy',
-    description: 'Finest fish and veggies',
-    backgroundImage: 'url("/path/to/image.jpg")',
+    description: 'Brandy is a spirit made from fermented fruit, typically grapes, and then aged in oak barrels. It has a rich and complex flavor and is often enjoyed as an after-dinner drink. Brandy is also used in cooking and baking.',
+    backgroundImage: 'url("src/Assets/brandy.png")',
   },
 ];
 
@@ -60,9 +61,10 @@ const SectionContents = () => {
       {sectionData.map(({ id, name, description, backgroundImage }) =>
         id === activeSection ? (
           <div key={id}>
+            <img className={classes['main-image']} src={backgroundImage} alt='glass of brandy'/>
             <h2>{name}</h2>
             <p>{description}</p>
-            <div style={{ backgroundImage }}></div>
+
           </div>
         ) : null
       )}
