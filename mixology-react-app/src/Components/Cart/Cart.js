647a68f3928c82b9a9835eda04434473cf0572ba
makoma mabilo <makoma44@gmail.com>
2023-04-26 13:08:24 +0800

    displaying cartItems

--- mixology-react-app/src/Components/Cart/Cart.js
+++ mixology-react-app/src/Components/Cart/Cart.js
@@ -1,31 +1,43 @@
+import { useContext } from 'react';
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
+import CartContext from './../../Store/Cart-Context'
+import CartItem from './CartItem';
+
 
 
 const Cart = props => {
+ const cartCtx = useContext(CartContext);
+ const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+ const hasItems = cartCtx.items.length > 0;
+
+ const cartItemRemoveHandler = (id) => {};
+
+ const cartItemAddHandler = (item) => {};
 
   const cartItems = (
     <ul className={classes['cart-items']}>
-      {[{ id: 'd1',
-    sectionId: 's1',
-    amount: 2,
-    name: 'Gin and Tonic',
-    price: 16.99}].map((item) => (
-      <li>{item.name}</li>
+      {cartCtx.items.map((item) => (
+       <CartItem key={item.id}
+        name={item.name}
+        amount={item.amount}
+        price={item.price}
+        onRemove={cartItemRemoveHandler.bind(null,item)}
+        onAdd={cartItemAddHandler.bind(null,item)}/>
       ))}
     </ul>
   );
 
   return (
     <Modal onClose={props.onClose}>
-       {cartItems }
+      {cartItems}
        <div>
        <span>Total Amount</span>
-       <span>24.89</span>
+       <span>{totalAmount}</span>
        </div>
         <div className={classes.actions}>
          <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
-         <button className={classes.button}>Order</button>
+         {hasItems && <button className={classes.button}>Order</button>}
        </div>
     </Modal>
   )
