daba2ffa42b71f479bc26085f61999ebaf9673fa
makoma mabilo <makoma44@gmail.com>
2023-05-06 14:12:00 +0800

    making modal buttons functional to delete and add items

--- mixology-react-app/src/Store/CartProvider.js
+++ mixology-react-app/src/Store/CartProvider.js
@@ -3,19 +3,18 @@ import CartContext from "./Cart-Context";
 
 const defaultCartState = {
   items: [],
-  totalAmount: 0,
+  totalAmount:0
 };
 
-const cartReducer = (state, action) => {
-  if (action.type === 'ADD') {
-    const updatedTotalAmount =
-      state.totalAmount + action.item.price * action.item.amount;
+const cartReducer = (state,action) => {
+  if(action.type === 'ADD') {
 
-    const existingCartItemIndex = state.items.findIndex(
-      (item) => item.id === action.item.id
-    );
+    const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+    const existingCartItemIndex = state.items.findIndex(item => item.id === action.item.id);
+
     const existingCartItem = state.items[existingCartItemIndex];
-    let updatedItems;
+
+     let updatedItems;
 
     if (existingCartItem) {
       const updatedItem = {
@@ -24,20 +23,23 @@ const cartReducer = (state, action) => {
       };
       updatedItems = [...state.items];
       updatedItems[existingCartItemIndex] = updatedItem;
-    } else {
+    }
+     else {
       updatedItems = state.items.concat(action.item);
     }
 
     return {
-      items: updatedItems,
-      totalAmount: updatedTotalAmount,
-    };
+    items:updatedItems,
+    totalAmount: updatedTotalAmount
+   };
   }
-  if (action.type === 'REMOVE') {
+
+   if (action.type === 'REMOVE') {
     const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.id
     );
-    const existingItem = state.items[existingCartItemIndex];
+
+   const existingItem = state.items[existingCartItemIndex];
     const updatedTotalAmount = state.totalAmount - existingItem.price;
     let updatedItems;
     if (existingItem.amount === 1) {
@@ -47,42 +49,40 @@ const cartReducer = (state, action) => {
       updatedItems = [...state.items];
       updatedItems[existingCartItemIndex] = updatedItem;
     }
-
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount
     };
-  }
 
-  return defaultCartState;
+   }
+  return defaultCartState
 };
 
-const CartProvider = (props) => {
-  const [cartState, dispatchCartAction] = useReducer(
-    cartReducer,
-    defaultCartState
-  );
+const CartProvider = props => {
+  const[cartState,dispatchCartAction] = useReducer(cartReducer, defaultCartState);
 
-  const addItemToCartHandler = (item) => {
-    dispatchCartAction({ type: 'ADD', item: item });
+  const addItemHandler = (item) => {
+    dispatchCartAction({type:'ADD', item:item});
   };
 
-  const removeItemFromCartHandler = (id) => {
-    dispatchCartAction({ type: 'REMOVE', id: id });
+  const removeItemHandler = (id) => {
+    dispatchCartAction({type:'REMOVE', id:id});
   };
 
   const cartContext = {
-    items: cartState.items,
+    items:cartState.items,
     totalAmount: cartState.totalAmount,
-    addItem: addItemToCartHandler,
-    removeItem: removeItemFromCartHandler,
-  };
+    addItem:addItemHandler,
+    removeItem:removeItemHandler,
+  }
 
-  return (
-    <CartContext.Provider value={cartContext}>
-      {props.children}
-    </CartContext.Provider>
-  );
+
+
+  return <CartContext.Provider value={cartContext}>
+    {props.children}
+
+  </CartContext.Provider>
+
 };
 
 export default CartProvider;
